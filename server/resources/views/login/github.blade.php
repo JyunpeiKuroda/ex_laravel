10c0401a2a153bfada6b03a96a1737def1a4e758
JyunpeiKuroda <totoro.black6996@gmail.com>
2020-03-04 02:38:16 +0900

    #12020-03-04-02:38

--- server/resources/views/login/github.blade.php
+++ server/resources/views/login/github.blade.php
@@ -1,1 +1,14 @@
-<button>GitHubでログイン</button>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="css/login.css">
+  </head>
+  <body>
+    <div class="login-box">
+      <div class="login-small-box">
+        <button>GitHubでログイン</button>
+      </div>
+    </div>
+  </body>
+</html>
