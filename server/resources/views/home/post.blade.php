41a082d17c458bbb9bb48fc2aef8d035b4b3aae4
JyunpeiKuroda <totoro.black6996@gmail.com>
2020-03-04 10:42:06 +0900

    #12020-0304-10:40
    ・post.blade.php投稿画面
    ・投稿画面、ボタンのスタイル(中央部)

--- server/resources/views/home/post.blade.php
+++ server/resources/views/home/post.blade.php
@@ -1,1 +1,14 @@
-<h1>投稿画面</h1>
+@extends('layout')
+@section('title', '投稿')
+@section('content')
+<div class="form_box">
+    <p>投稿画面のページです</p>
+    <form action="post_exe.php" method="post">
+      <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg">
+    <div class="form_item"><input type="submit" value="写真を選択する" class="btn btn-primary"></div><br>
+    <textarea name="comment" rows="7" cols="40" maxlength="200" ></textarea>
+    <br>
+    <div class="form_item"><input type="submit" value="登録する" class="btn btn-primary"></div>
+    <form>
+</div>
+@endsection
