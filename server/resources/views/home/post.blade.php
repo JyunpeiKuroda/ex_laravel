768f57c4e2d00b3e3b7ef0f8829f1754111824f3
JyunpeiKuroda <totoro.black6996@gmail.com>
2020-03-05 14:50:41 +0900

    #12020-03-05

--- server/resources/views/home/post.blade.php
+++ server/resources/views/home/post.blade.php
@@ -1,22 +1,26 @@
+<!DOCTYPE html>
 @extends('layout.h&f')
 @section('title', '投稿')
 @section('content')
-
-<div class="form_box">
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-  <script src="js/style.js" type="text/javascript"></script>
-    <form action="post_exe.php" method="post">
-      <h1>投稿画面です</h1><br>
-      <label class="input-btn">
-        <span class="btn btn-primary">
-          写真を選択<input type="file" onChange="imgPreView(event)" style="display:none">
-        </span>
-      </label>
-      <div id="preview"></div><br>
-      <textarea name="comment" rows="7" cols="40" maxlength="200" ></textarea>
-      <br>
-      <div class="form_item"><input type="submit" value="投稿" class="btn btn-primary"></div>
-    <form>
-</div>
+<html>
+  <body>
+    <div class="form_box">
+      <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+      <script src="js/style.js" type="text/javascript"></script>
+        <form action="post_exe.php" method="post">
+          <h1>投稿画面です</h1><br>
+          <label class="input-btn">
+            <span class="btn btn-primary">
+              写真を選択<input type="file" onChange="imgPreView(event)" style="display:none">
+            </span>
+          </label>
+          <div id="preview"></div><br>
+          <textarea name="comment" rows="7" cols="40" maxlength="200" ></textarea>
+          <br>
+          <div class="form_item"><input type="submit" value="投稿" class="btn btn-primary"></div>
+        <form>
+    </div>
+  </body>
+</html>
 @endsection
 
--- server/resources/views/top.blade.php
+++ server/resources/views/top.blade.php
@@ -1,6 +1,31 @@
+<!DOCTYPE html>
+
 @extends('layout.h&f')
 @section('title', 'トップページ')
 @section('content')
-トップページ
+<html>
+  <body>
+    <div class="top-big">
+      <div class="top-big-box">
+        <h1>トップページ</h1>
+        <div class="top-post-sheet">
+          <div class="post-sheet-title">
+            <div class="post-user-name"></div>
+            <button class= "delete">削除</button>
+          </div>
+
+          <div class="post-img">
+            <div class="post-sheet-img"></div>
+          </div>
+
+          <div class="post-sheet-text">
+            <div class="post-text"></div>
+            <button class="post-like">イイネ</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
 <a href="{{ url('signup') }}">新規登録ページへ</a>
 @endsection
