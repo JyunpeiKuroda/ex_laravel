4dfac31fba1ceaba1869a682e17252a11f20dfcd
JyunpeiKuroda <52275136+JyunpeiKuroda@users.noreply.github.com>
2020-03-22 20:36:13 +0900

    Revert "fast-create"

--- server/routes/web.php
+++ server/routes/web.php
@@ -10,14 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-// 最初のウェルカムページのルート
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/', 'HomeController@index');        #ホーム画面のpathのroute
-Route::resource('posts', 'PostController');     #投稿画面のpathのroute
-Route::get('/like', 'LikeController@index');     #イイネ一覧画面のpathのroute
-Route::get('/github', 'LoginController@index');  #githubでのログイン画面のpathのroute
 
-Route::get('/top','TopController@index');  #css適用確認用
-Route::get('/signup','SignupController@index');  #css適用確認用  投稿画面に変更？
+Route::get('/', function () {
+    return view('welcome');
+});
